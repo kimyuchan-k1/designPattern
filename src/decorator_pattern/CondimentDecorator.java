7b1097c560c1598d088246ffa157dccff53bccfc
kimyuchan <rladbcks103@naver.com>
2025-01-18 17:25:02 +0900

    decorator pattern - StarbuzzCoffee implement

---
+++ src/decorator_pattern/CondimentDecorator.java
@@ -0,0 +1,7 @@
+package decorator_pattern;
+
+public abstract class CondimentDecorator extends Beverage {
+    Beverage beverage;
+
+    public abstract String getDescription();
+}
---
+++ src/decorator_pattern/Whip.java
@@ -0,0 +1,16 @@
+package decorator_pattern;
+
+public class Whip extends CondimentDecorator{
+    public Whip(Beverage beverage){
+        this.beverage = beverage;
+    }
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", 휘핑크림";
+    }
+
+    @Override
+    public double cost() {
+        return beverage.cost() + .21;
+    }
+}
